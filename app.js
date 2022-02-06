f958fcd67923c67ac998f1e13d456f468b1e759e
Mark <bmwark@gmail.com>
2022-02-06 22:41:23 +0300

    update port

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.action('button_click', async ({ body, ack, say }) => {
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 5000);
 
   console.log('⚡️ Bolt app is running!');
 })();
