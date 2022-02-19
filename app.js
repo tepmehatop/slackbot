a350d754b61c4f3ff531d682918555c910780c2a
Mark <bmwark@gmail.com>
2022-02-19 09:35:28 +0300

    inital 2

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = new App({
 
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
+  // say() sends a message to the channel where the event was triggered 
   await say({
     blocks: [
       {
